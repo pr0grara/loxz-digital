e1a690ac6878f80cb778fd0cebcb2041b02ebbc3
Ara Z Baghdassarian <azbaghda@gmail.com>
2021-04-16 19:31:53 -0700

    deploy intro to main survey

--- client/src/components/landing/landing.jsx
+++ client/src/components/landing/landing.jsx
@@ -30,8 +30,8 @@ class Landing extends React.Component {
         // closeButton.className = 'close';
         // closeButton.innerText = 'close';
         const reference = typeformEmbed.createSlider(
-           'AhCdegfT',
-        //    'xF6ZyWUR',
+        //    'AhCdegfT', //secondary
+           'xF6ZyWUR', //primary
            {
                mode: 'drawer_left',
             //    autoClose: 5,
